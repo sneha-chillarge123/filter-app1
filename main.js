923691fac3cd9c7ff47c8e28a430d71c94e9a3c4
sneha-chillarge123 <95573561+sneha-chillarge123@users.noreply.github.com>
2022-07-01 17:54:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(400,300);
+    canvas.position(480,250);
+}
+
+function draw(){
+
+}
+
+function takeSnapshot(){
+    save("image.png");
+}
